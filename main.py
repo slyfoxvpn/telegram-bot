7ab3105d5cc1e3c1c712ce1dc242192bf895021f
xelond-dev <xelond@proton.me>
2023-12-11 01:33:04 +0300

    Update usages

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-import OutlineVPN_Key_Managament as keys
+import libs.Outline_Key_Manager as okm
 import urllib3
 from config_reader import config
 
-urllib3.disable_warnings() # But maybe it is need to fix
+urllib3.disable_warnings() # It is need to fix?
 
-# keys.init(config.outline_api.get_secret_value()) # Init
-# keys.get_all_keys() # Get all keys
-# keys.new_key("Example1") # Create new key
-# keys.get_key("Example1") # Get information about key
-# keys.rename_key("Example1", "NewExampleName") # Rename from Example1 to NewExampleName
-# keys.set_limit("NewExampleName", 2048) # Set limit to 2GB
-# keys.remove_limit("NewExampleName")
-# keys.remove_key("NewExampleName")
-
-# print(keys.get_all_keys())
-
-
+okm.init(config.outline_api.get_secret_value(), debug=True) # Init
+okm.get_all_keys() # Get all keys
+okm.new_key(name="Example1") # Create new key
+okm.get_key("Example1") # Get information about key
+okm.rename_key("Example1", "NewExampleName") # Rename from Example1 to NewExampleName
+okm.set_limit("NewExampleName", 2048) # Set limit to 2GB
+okm.remove_limit("NewExampleName") # Remove limit
+okm.remove_key("NewExampleName") # Remove key
