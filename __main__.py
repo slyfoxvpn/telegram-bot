e873ae2eddbbec1ca1a580825e31a2689d610362
Sovamm <sovam.main@gmail.com>
2023-12-10 21:09:09 +0300

    refactor start functions

--- __main__.py
+++ __main__.py
@@ -17,48 +17,36 @@ bot = Bot(token=config.bot_token.get_secret_value(), parse_mode="Markdown") # Bo
 dp = Dispatcher() # Dispatcher (?)
 
 
-# 'start' with auth code (if exist)
-@dp.message(F.text, CommandStart(deep_link=True, magic=F.args.regexp(re.compile(r'auth_(\d+)'))))
-async def cmd_start(message: Message, command: CommandObject):
+# Main start function
+async def start_main(message: Message, auth_id=None, ref_id=None):
+    if auth_id:
+        await message.answer(f"Auth with {auth_id}")
+    elif ref_id:
+        await message.answer(f"Ref from {ref_id}")
+    else:
+        keyboard = types.ReplyKeyboardMarkup(keyboard=[
+            [types.KeyboardButton(text="❔❓Как это сделать?")],
+        ], resize_keyboard=True, input_field_placeholder="Денис денисочка")
+
+        await message.answer("*Привет! На связи команда SlyFox.\nДавайте найдем ваш аккаунт.*")
+        await message.answer("🦊", reply_markup=keyboard)
+
+
+# Start functions dor sereval cases
+@dp.message(F.text, CommandStart(deep_link=True, magic=F.args.regexp(re.compile(r'auth_(\d+)')))) # Start with auth id
+async def start_auth(message: Message, command: Command):
     auth_id = command.args.split("_")[1]
-    await message.answer(f"Auth with {auth_id}")
+    await start_main(message, auth_id=auth_id)
 
-
-# 'start' with ref code (if exist)
-@dp.message(F.text, CommandStart(deep_link=True, magic=F.args.regexp(re.compile(r'ref_(\d+)'))))
-async def cmd_start(message: Message, command: CommandObject):
+@dp.message(F.text, CommandStart(deep_link=True, magic=F.args.regexp(re.compile(r'ref_(\d+)')))) # Start with ref id
+async def start_ref(message: Message, command: Command):
     ref_id = command.args.split("_")[1]
-    await message.answer(f"Ref from {ref_id}")
+    await start_main(message, ref_id=ref_id)
 
-
-# 'start' default handler
 @dp.message(F.text, CommandStart())
-async def cmd_start(message: types.Message):
-    keyboard = types.ReplyKeyboardMarkup(keyboard=[
-        [types.KeyboardButton(text="❔❓Как это сделать?")],
-    ], resize_keyboard=True, input_field_placeholder="Денис денисочка")
+async def start_default(message: types.Message): # Start for other cases
+    await start_main(message)
 
-    await message.answer("*Привет! На связи команда SlyFox.\nДавайте найдем ваш аккаунт.*")
-    await message.answer("🦊", reply_markup=keyboard)
-
-
-# Я хотел объединить это, но чет не работает
-# @dp.message(CommandStart(deep_link=True, magic=F.args.regexp(re.compile(r'auth_(\d+)'))))
-# async def cmd_start(message: types.Message, command: None):
-#     if command and command.args:
-#         auth_number = command.args.split("_")[1]
-#         await message.answer(f"*Вы успешно авторизовались, используя код {auth_number}.*")
-#     else:
-#         await message.answer("*Привет! На связи команда SlyFox.*")
-
-#     await message.answer("🦊")
-
-
-# @dp.message(F.text, Command("admin"))
-# async def adm(message: types.Message):
-#     if (message.chat.id == 1826617805):
-#         await message.answer("Введите код администратора:")
-        
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
