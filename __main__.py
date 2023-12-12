4e27164e65c07f64cd54046a9d4dd81f2ece9fdc
xelond-dev <xelond@proton.me>
2023-12-12 20:26:57 +0300

    Fix wrong builder name

--- __main__.py
+++ __main__.py
@@ -79,7 +79,7 @@ async def callback_query_handler_all_product(callback_query: types.CallbackQuery
 # Callback query for germany_srv
 @dp.callback_query(lambda c: c.data == 'germany_srv')
 async def callback_query_handler_germany_srv(callback_query: types.CallbackQuery):
-    keyboard = builders.keyboardBuilder(country_flag="🇩🇪", country_name="germany")
+    keyboard = builders.keyboard_builder(country_flag="🇩🇪", country_name="germany")
     await bot.edit_message_text(text="*Список доступных тарифов на локации 🇩🇪 Германия, Франкфурт:*", reply_markup=keyboard, chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id)
     await bot.answer_callback_query(callback_query.id)
 
